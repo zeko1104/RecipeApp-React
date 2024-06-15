b95355215572707c426c68f548ee87d85cc41273
Zeko706 <131792180+Zeko706@users.noreply.github.com>
2024-06-15 14:35:28 +0400

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/navbar/recipe-item";
+
+export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something!
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
